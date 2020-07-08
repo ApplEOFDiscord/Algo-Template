9143132dec3aabfeb903e886a7fabda85dfcd049
ApplEOFDiscord <wwy640130@163.com>
2020-07-08 19:58:50 +0800

    fix typo

--- Math/lagrange.cpp
+++ Math/lagrange.cpp
@@ -37,13 +37,13 @@ LL pre[2010],suf[2010],inv[2010];
 LL lagrange(int n,LL *x,LL *y,LL v)
 {
     LL res=0;
-    pre[0]=(v-x[0]+mod)%mod,suf[n]=1;
-    for(int i=1;i<n;i++) pre[i]=pre[i-1]*(v-x[i])%mod;
-    for(int i=n-1;i>=0;i--) suf[i]=suf[i+1]*(v-x[i])%mod;
+    pre[0]=((v-x[0])%mod+mod)%mod,suf[n]=1;
+    for(int i=1;i<n;i++) pre[i]=pre[i-1]*(v-x[i])%mod,pre[i]=(pre[i]+mod)%mod;
+    for(int i=n-1;i>=0;i--) suf[i]=suf[i+1]*(v-x[i])%mod,suf[i]=(suf[i]+mod)%mod;
     inv[0]=inv[1]=1;
     for(int i=2;i<n;i++) inv[i]=(mod-mod/i)*inv[mod%i]%mod;
     for(int i=2;i<n;i++) inv[i]=inv[i]*inv[i-1]%mod;
-    for(int i=0;i<n;i++) res+=y[i]*(i==0?1:pre[i-1])%mod*suf[i+1]%mod*inv[i]%mod*(((n-i)&1)?mod-1:1)*inv[n-i]%mod,res%=mod;
+    for(int i=0;i<n;i++) res+=y[i]*(i==0?1:pre[i-1])%mod*suf[i+1]%mod*inv[i]%mod*(((n-1-i)&1)?mod-1:1)%mod*inv[n-1-i]%mod,res%=mod;
     return res;
 }*/
 
